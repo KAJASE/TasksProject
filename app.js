6bf144850d3b9dbb39ebd9a09c4691f5316cfa4e
Karen <kareninejapiassu@gmail.com>
2021-02-22 17:50:02 +0100

    DeleteFilterTasks

--- app.js
+++ app.js
@@ -7,8 +7,13 @@ const taskInput = document.querySelector('#task');
 // Load all event listeners
 loadEventListeners();
 
+
+
 function loadEventListeners(){
   form.addEventListener("submit", addTask);
+  taskList.addEventListener('click', removeTasks);
+  clearBtn.addEventListener('click', clearTasks);
+  filter.addEventListener('keyup', filterTasks);
 }
 
 function addTask(e){
@@ -27,4 +32,29 @@ function addTask(e){
   taskList.appendChild(li);
   taskInput.value = '';
   e.preventDefault();
+}
+
+function removeTasks(e){
+  if(e.target.parentElement.classList.contains('delete-item')){
+    e.target.parentElement.parentElement.remove();
+  }
+}
+
+function clearTasks(e){
+  //taskList.innerHTML = '';
+  while(taskList.firstChild){
+    taskList.removeChild(taskList.firstChild);
+  }
+}
+
+function filterTasks(e){
+  const text = e.target.value.toLowerCase();
+  document.querySelectorAll('.collection-item').forEach(function(task){
+    const item = task.firstChild.textContent;
+    if(item.toLowerCase().indexOf(text) != -1){
+      task.style.display = 'block';
+    }else{
+      task.style.display = 'none';
+    }
+  });
 }
